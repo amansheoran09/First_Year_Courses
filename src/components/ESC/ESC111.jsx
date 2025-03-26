2f00a09ba3824b8053b2a2ef34a9ab3828567e02
amansheoran09 <amansheoran751@gmail.com>
2025-03-26 23:23:31 +0530

    Update ESC111.jsx

--- src/components/ESC/ESC111.jsx
+++ src/components/ESC/ESC111.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const ESC111 = () => {
   return (
-       <div><Link target='_blank' to="/esc111" className='link'><button className="card card1">ESC111</button></Link></div>
+       <div><Link to="/esc111" className='link'><button className="card card1">ESC111</button></Link></div>
   )
 }
 
